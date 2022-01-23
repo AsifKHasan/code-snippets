998353e749645585bb2e38bd05f4836feb4df23d
Asif Hasan <asif.hasan@gmail.com>
2022-01-23 22:42:59 +0600

    project refactoring

---
+++ secl-tools/document-from-data/src/document-from-data.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+    generate templated document from data
+'''
+
+def process():
+    # get the configuration
+
+    # get to Google
+
+    # get the gsheet
+
+    # load the appropriate gsheet data parser module
+
+    # parse the data
+
+    # load the appropriate gsheet data parser module
--- secl-tools/salary-revision/src/gsheet-to-odt.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-'''
-    generate odt (OpenDocument Text) document from gsheet data and odt templates
-'''
