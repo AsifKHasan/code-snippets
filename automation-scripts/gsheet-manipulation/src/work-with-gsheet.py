9acc870a5411ff70e5c5e71084d0930fb296081e
Asif Hasan <asif.hasan@gmail.com>
2023-05-02 01:49:14 +0600

    updated

--- automation-scripts/gsheet-manipulation/src/work-with-gsheet.py
+++ automation-scripts/gsheet-manipulation/src/work-with-gsheet.py
@@ -29,16 +29,16 @@ def work_on_gsheet(g_sheet, g_service, worksheet_names, destination_gsheet_names
     # g_sheet.rename_worksheet(worksheet_name='00-layout', new_worksheet_name='00-layout-WB')
 
     # worksheet creation, formatting and related tasks
-    g_sheet.clear_conditional_formats(worksheet_names=worksheet_names)
+    # g_sheet.clear_conditional_formats(worksheet_names=worksheet_names)
     # g_sheet.create_review_notes_conditional_formatting(worksheet_names=worksheet_names)
 
-    g_sheet.format_worksheets(worksheet_names=worksheet_names)
+    # g_sheet.format_worksheets(worksheet_names=worksheet_names)
     # create_worksheets(g_sheet=g_sheet, worksheet_name_list=worksheet_names)
 
     # trailing blank row removal, review-notes, column size in row 1
     # g_sheet.remove_extra_columns(worksheet_names=worksheet_names, cols_to_remove_from='F', cols_to_remove_to='end')
     # g_sheet.remove_trailing_blank_rows(worksheet_names=worksheet_names)
-    g_sheet.column_pixels_in_top_row(worksheet_names=worksheet_names)
+    # g_sheet.column_pixels_in_top_row(worksheet_names=worksheet_names)
 
     # work on ranges etc.
     # g_sheet.work_on_ranges(worksheet_names=worksheet_names, range_work_specs=work_specs)
