80ada05d6bc3e2a29f4eb9a8bbb2cac9b4860e86
Asif Hasan <asif.hasan@gmail.com>
2023-09-30 23:39:26 +0600

    updated

--- automation-scripts/gsheet-manipulation/src/work-with-gsheet.py
+++ automation-scripts/gsheet-manipulation/src/work-with-gsheet.py
@@ -68,10 +68,10 @@ def work_on_gsheet(g_sheet, g_service, worksheet_names, destination_gsheet_names
 
 
     # cell linking and ordering
-    # g_sheet.link_cells_based_on_type(worksheet_name='-toc-new', range_specs_for_cells_to_link=['E3:F'])
-    # g_sheet.link_cells_to_worksheet(worksheet_name='-toc-new', range_specs_for_cells_to_link=['O3:O', 'R3:R'])
+    g_sheet.link_cells_based_on_type(worksheet_name='-toc-new', range_specs_for_cells_to_link=['E3:F'])
+    g_sheet.link_cells_to_worksheet(worksheet_name='-toc-new', range_specs_for_cells_to_link=['O3:O', 'R3:R'])
     # g_sheet.link_cells_to_drive_files(worksheet_name='-toc-new', range_specs_for_cells_to_link=[])
-    # g_sheet.order_worksheets()
+    g_sheet.order_worksheets()
 
 
     # copy worksheets to another gsheet
