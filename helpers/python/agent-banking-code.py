079c6b2344fc903a57fed9fc09eaed788de70c20
Asif Hasan <asif.hasan@gmail.com>
2023-10-27 04:21:22 +0600

    updated

--- helpers/python/agent-banking-code.py
+++ helpers/python/agent-banking-code.py
@@ -2,27 +2,42 @@ import numpy as np
 import pandas as pd
 from plotnine import *
 
-# theme_set(theme_538())
-# theme_set(theme_dark())
-# theme_set(theme_bw())
-# theme_set(theme_classic())
-# theme_set(theme_dark())
-# theme_set(theme_gray())
-# theme_set(theme_light())
-# theme_set(theme_linedraw())
-# theme_set(theme_matplotlib())
-# theme_set(theme_minimal())
-# theme_set(theme_seaborn())
-# theme_set(theme_tufte())
-# theme_set(theme_void())
-# theme_set(theme_xkcd())
+data_path = "/home/asif/projects/asif@github/code-snippets/helpers/R/agent-banking-data.csv"
+data_path = "D:/projects/asif@github/code-snippets/helpers/R/agent-banking-data.csv"
+
+image_dir = "C:/Users/Asif/Downloads"
+
+last_q = '2023-Q2'
+
 
 theme_set(theme_538())
 
-data = pd.read_csv("/home/asif/projects/asif@github/code-snippets/helpers/R/agent-banking-data.csv", sep='\t')
+all_data = pd.read_csv(data_path, sep='\t', thousands=',')
+
+# the latest data is the last quarter data
+latest_data = all_data[all_data['quarter'] == last_q] 
 
 # Rural/Urban outlet ratio
+data = latest_data[['code', 'outlets-urban', 'outlets-rural']]
+data["outlets-ratio"] = data['outlets-rural'] / data['outlets-urban']
+
+# the axes
 x = 'code'
 y = 'outlets-ratio'
-p1 = (ggplot(data, aes(x=x, y=y)) + geom_point() + geom_segment(aes(x=x, xend=x, y=0, yend=y)))
+
+top_values_to_select = 10
+bottom_values_to_select = len(data) - top_values_to_select
+
+# top banks
+p1 = (ggplot(data.nlargest(top_values_to_select, 'outlets-ratio'), aes(x=x, y=y)) + geom_point() + geom_segment(aes(x=x, xend=x, y=0, yend=y)))
 p1 = p1 + theme(axis_text_x=element_text(family="Arial", weight="light", style="normal", size=7, color="black", angle=45, hjust=1)) + xlab("Banks") + ylab("Rural/Urban outlet ratio")
+
+p1_path = f"{image_dir}/outlets-ratio__cumulative__top-{top_values_to_select}-banks.png"
+p1.save(filename=p1_path)
+
+# bottom banks
+p2 = (ggplot(data.nsmallest(bottom_values_to_select, 'outlets-ratio'), aes(x=x, y=y)) + geom_point() + geom_segment(aes(x=x, xend=x, y=0, yend=y)))
+p2 = p2 + theme(axis_text_x=element_text(family="Arial", weight="light", style="normal", size=7, color="black", angle=45, hjust=1)) + xlab("Banks") + ylab("Rural/Urban outlet ratio")
+
+p2_path = f"{image_dir}/outlets-ratio__cumulative__bottom-{bottom_values_to_select}-banks.png"
+p2.save(filename=p2_path)
---
+++ helpers/python/ggplot_helper.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from plotnine import *
+
+# theme_set(theme_538())
+# theme_set(theme_dark())
+# theme_set(theme_bw())
+# theme_set(theme_classic())
+# theme_set(theme_dark())
+# theme_set(theme_gray())
+# theme_set(theme_light())
+# theme_set(theme_linedraw())
+# theme_set(theme_matplotlib())
+# theme_set(theme_minimal())
+# theme_set(theme_seaborn())
+# theme_set(theme_tufte())
+# theme_set(theme_void())
+# theme_set(theme_xkcd())
