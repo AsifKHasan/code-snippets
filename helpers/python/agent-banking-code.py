f8843e553dfa4405cb83a3eeb5a71e9321a474e1
Asif Hasan <asif.hasan@gmail.com>
2023-10-27 11:18:30 +0600

    updated

--- helpers/python/agent-banking-code.py
+++ helpers/python/agent-banking-code.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from plotnine import *
 
-data_path = "/home/asif/projects/asif@github/code-snippets/helpers/R/agent-banking-data.csv"
+# data_path = "/home/asif/projects/asif@github/code-snippets/helpers/R/agent-banking-data.csv"
 data_path = "D:/projects/asif@github/code-snippets/helpers/R/agent-banking-data.csv"
 
 image_dir = "C:/Users/Asif/Downloads"
@@ -17,7 +17,8 @@ all_data = pd.read_csv(data_path, sep='\t', thousands=',')
 # the latest data is the last quarter data
 latest_data = all_data[all_data['quarter'] == last_q] 
 
-# Rural/Urban outlet ratio
+###
+# BEGIN Rural/Urban outlet ratio
 data = latest_data[['code', 'outlets-urban', 'outlets-rural']]
 data["outlets-ratio"] = data['outlets-rural'] / data['outlets-urban']
 
@@ -28,16 +29,37 @@ y = 'outlets-ratio'
 top_values_to_select = 10
 bottom_values_to_select = len(data) - top_values_to_select
 
-# top banks
-p1 = (ggplot(data.nlargest(top_values_to_select, 'outlets-ratio'), aes(x=x, y=y)) + geom_point() + geom_segment(aes(x=x, xend=x, y=0, yend=y)))
-p1 = p1 + theme(axis_text_x=element_text(family="Arial", weight="light", style="normal", size=7, color="black", angle=45, hjust=1)) + xlab("Banks") + ylab("Rural/Urban outlet ratio")
+color_dict = {'Agrani': '#5b0f00'}
+colors = {code: color_dict.get(code, '#434343') for code in data['code'].tolist()}
+# radius = {code:  for code in data['code'].tolist()}
 
+# top N banks
+p1 = ggplot(data.nlargest(top_values_to_select, 'outlets-ratio'), aes(x=x, y=y, color='code')) + \
+    geom_point() + \
+    geom_segment(aes(x=x, xend=x, y=0, yend=y)) + \
+    geom_text(aes(label=y), size=7, angle=30, format_string="{:.2f}", nudge_x=0.3, nudge_y=5, family="Arial", fontweight="light", fontstyle="normal") + \
+    scale_color_manual(values=colors) + \
+    guides(color = False, size = False)
+
+p1 = p1 + theme(
+                axis_text_x=element_text(family="Arial", weight="light", style="normal", size=8, color="black", angle=45, hjust=1)
+            ) + \
+    xlab("Banks") + \
+    ylab("Rural/Urban outlet ratio")
+
+# save as image
 p1_path = f"{image_dir}/outlets-ratio__cumulative__top-{top_values_to_select}-banks.png"
 p1.save(filename=p1_path)
 
-# bottom banks
+
+# bottom M banks
 p2 = (ggplot(data.nsmallest(bottom_values_to_select, 'outlets-ratio'), aes(x=x, y=y)) + geom_point() + geom_segment(aes(x=x, xend=x, y=0, yend=y)))
 p2 = p2 + theme(axis_text_x=element_text(family="Arial", weight="light", style="normal", size=7, color="black", angle=45, hjust=1)) + xlab("Banks") + ylab("Rural/Urban outlet ratio")
 
+# save as image
 p2_path = f"{image_dir}/outlets-ratio__cumulative__bottom-{bottom_values_to_select}-banks.png"
 p2.save(filename=p2_path)
+# END Rural/Urban outlet ratio
+###
+
+
