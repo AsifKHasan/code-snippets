c037bc78d7ae5fcc17bb3b2be89e7153ed8c2af0
Asif Hasan <asif.hasan@gmail.com>
2024-03-07 06:05:48 +0600

    updated

--- codes/python/ftp-helper.py
+++ codes/python/ftp-helper.py
@@ -30,11 +30,11 @@ things_we_have = ftp.nlst()
 # output_list = list(set(things_we_have).intersection(things_we_need))
 # print(f"we have {len(output_list)} correct things")
 
-# output_list = list(set(things_we_have) - set(things_we_need))
-# print(f"we have {len(output_list)} extra/wrong things")
+output_list = list(set(things_we_have) - set(things_we_need))
+print(f"we have {len(output_list)} extra/wrong things")
 
-output_list = list(set(things_we_need) - set(things_we_have))
-print(f"we have [{len(output_list)}] missing {artifact}s for [{org}] in ftp [{ftp_directory}]")
+# output_list = list(set(things_we_need) - set(things_we_have))
+# print(f"we have [{len(output_list)}] missing {artifact}s for [{org}] in ftp [{ftp_directory}]")
 
 output_list.sort()
 for i in range(0, len(output_list)):
